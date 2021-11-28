8b3c0ec004f626299d46affa30eb58781cd1d95f
Douglas Santiago <douglassantiagods@hotmail.com>
2021-11-28 19:08:57 -0300

    :tada: Challenge Finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  selectedGenre: { title: string };
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Runtime: string;
+    Ratings: Array<{ value: string }>;
+  }>  
+}
+
+export function Content({movies, selectedGenre}: MovieProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard 
+              key ={movie.Title}
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].value} 
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
